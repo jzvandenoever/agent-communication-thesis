ff3869475e69d568a6de0dc6b89fd4e8a6da3dc5
Joris Zimrilim van den Oever <joris@vdoever.eu>
2016-03-23 23:59:58 +0100

    First attempt at block prediction
    Still not perfect but usually works without breaking anything at this
    point.
    
    Also started communicating which rooms are visited.

--- agents/common/commonKnowledge.pl
+++ agents/common/commonKnowledge.pl
@@ -1,5 +1,6 @@
 :- dynamic(state/1).
 :- dynamic(in/1).
+:- dynamic(at/1).
 :- dynamic(visited/1).
 :- dynamic(zone/5).
 :- dynamic(block/3).
@@ -9,13 +10,18 @@
 :- dynamic(seqDone/1).
 :- dynamic(sequenceIndex/1).
 :- dynamic(send/2).
+:- dynamic(ahead/1).
 
 
-% A room is a place with exactly one neighbour, i.e., there is only one way to get to and from that place.
+% A room is a place with exactly one neighbour, i.e., there is only one way to get 
+% to and from that place.
 room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
 dropZone('DropZone').
 
 % Block information.
 holdingNextBlock :- holding(BlockID),block(BlockID, ColorID, Place), nextNeededColor(ColorID).
+holdingNextBlock(N) :- holding(BlockID),block(BlockID, ColorID, Place), nextNeededColor(ColorID, N).
 nextNeededColor(ColorID) :- sequence(Seq), seqDone(SDone), append(SDone, [ColorID|_], Seq).
+nextNeededColor(ColorID, N) :- sequence(Seq), seqDone(SDone), length(SDone, Len), M is Len + N, 
+	nth1(M, Seq, ColorID).
 finished :- sequence(Seq), seqDone(Seq).
