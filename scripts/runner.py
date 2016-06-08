40803efdef9d80bbb67a3bc5df822b1554664f6d
Joris Zimrilim van den Oever <joris@vdoever.eu>
2016-06-08 17:16:22 +0200

    Commit the actual runner script improvements.

--- scripts/runner.py
+++ scripts/runner.py
@@ -3,8 +3,11 @@ import subprocess
 import shlex
 import os
 import shutil
+import tempfile
+import re
 
 FAILURES = [0, 5, 25, 50, 75, 95, 100]
+AGENT_COUNTS = [3, 5, 10]
 CMD = 'java -cp {runtime} goal.tools.Run "{mas}" --repeats {repeats} --timeout {timeout} ' \
       '--dropchance {failure}'
 BW4T_CMD = 'java -jar {bw4t}'
@@ -12,6 +15,27 @@ DEFAULT_RUNTIME = 'runtime.jar'
 DEFAULT_BW4T = 'bw4t.jar'
 DEFAULT_REPEATS = 1 # No repeats
 DEFAULT_TIMEOUT = 300 # five minutes]
+
+
+def detect_early_stop(log_lines):
+    for logline in log_lines:
+        logline = logline.split()
+        if logline[2] != 'action':
+            break
+
+        agent_name = logline[3]
+        agent_action = logline[4]
+        agent_data = agents.get(agent_name, ('', False))
+        if agent_data[0] == agent_action:
+            agents[agent_name] = (agent_action, True)
+        elif 'collided' in agent_action:
+            pass
+        else:
+            agents[agent_name] = (agent_action, False)
+    # do stuff if all agents are repeating themselves.
+    if len(agents) == agent_count and all([data[1] for data in agents.values()]):
+        return True
+    return False
 
 
 if __name__ == "__main__":
@@ -31,36 +55,65 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     DEVNULL = open(os.devnull, 'wb')
-    for masfile in args.mas:
-        for fail_chance in FAILURES:
-            bw4t_path = os.path.dirname(args.bw4t)
-            bw4t_logs = bw4t_path.join('log')
-            if os.path.exists(bw4t_logs):
-                shutil.rmtree(bw4t_path.join('log'))
+    for maslocation in args.mas:
+        for agent_count in AGENT_COUNTS:
+            mas_file = open(maslocation, )
+            mas_contents = mas_file.readlines()
+            changed_mas = tempfile.NamedTemporaryFile(delete=False)
+            replacement_count = 'agentcount = "%s"' % agent_count
+            for line in mas_contents:
+                if 'agentcount' in line:
+                    line = re.sub(r'agentcount ?= ?["\']\d+["\']]', replacement_count, line)
+                changed_mas.write(line)
+            masfile = changed_mas.name
+            changed_mas.close()
+            for fail_chance in FAILURES:
+                bw4t_path = os.path.dirname(args.bw4t)
+                bw4t_logs = bw4t_path.join('log')
+                if os.path.exists(bw4t_logs):
+                    shutil.rmtree(bw4t_path.join('log'))
 
-            server = subprocess.Popen(shlex.split(BW4T_CMD.format(bw4t=args.bw4t)), stdout=DEVNULL,
-                                      stderr=subprocess.STDOUT)
-            client_command = CMD.format(runtime=args.runtime, repeats=args.repeats, mas=masfile,
-                                        timeout=args.timeout, failure=fail_chance)
-            # This way I don't need to check for spaces and stuff that needs escaping.
-            client = subprocess.Popen(shlex.split(client_command), stdout=subprocess.PIPE,
-                                      stderr=subprocess.STDOUT)
-            while True:
-                line = client.stdout.readline()
-                # Do stuff with the runtime output here.
-                poll = client.poll()
-                if line.endswith('milliseconds to run jobs.') or poll == None:
-                    print line, poll
-                    break
+                server = subprocess.Popen(shlex.split(BW4T_CMD.format(bw4t=args.bw4t)),
+                                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                # Make sure the server has started properly
+                while True:
+                    line = server.stdout.readline()
+                    if 'Launching the BW4T Server Graphical User Interface.' in line:
+                        break
 
-            client.terminate()
-            server.terminate()
-            client.communicate()
+                client_command = CMD.format(runtime=args.runtime, repeats=args.repeats, mas=masfile,
+                                            timeout=args.timeout, failure=fail_chance)
+                # This way I don't need to check for spaces and stuff that needs escaping.
+                client = subprocess.Popen(shlex.split(client_command), stdout=subprocess.PIPE,
+                                          stderr=subprocess.STDOUT)
+                # Setup the BW4T server log reading.
+                for file in os.listdir(bw4t_logs):
+                    if file.endswith(".log"):
+                        log_file = open(os.path.join(bw4t_logs, file))
+                agents = {}
+                while True:
+                    line = client.stdout.readline()
+                    serverlines = server.stdout.readline()
+                    # Do stuff with the runtime output here.
+                    log_lines = log_file.readlines()
+                    # Do cycle detection, and nothing happens detection.
+                    if args.repeats == 1 and detect_early_stop(log_lines):
+                        break
 
-            mas_name = os.path.basename(masfile)
-            os.renames(bw4t_logs, os.path.join(mas_name, '%.2f' % (fail_chance/100.0)))
-            print 'Ran {mas} with the following failure chance: {chance}'.format(mas=masfile,
-                                                                                 chance=fail_chance)
+                    poll = client.poll()
+                    if line.endswith('milliseconds to run jobs.') or poll == None:
+                        print line, poll
+                        break
+
+                client.terminate()
+                server.terminate()
+                client.communicate()
+
+                mas_name = os.path.basename(masfile)
+                os.renames(bw4t_logs, os.path.join(mas_name, '%.2f' % (fail_chance/100.0)))
+                print 'Ran {mas} with the following failure chance: {chance}'.format(mas=masfile,
+                                                                                     chance=fail_chance)
+            os.remove(changed_mas)
     print 'Finished doing runs.'
 
 
