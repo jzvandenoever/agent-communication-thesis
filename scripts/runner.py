06f889f5a055f6f2285afb31ad9bfdd8a4261959
Joris Zimrilim van den Oever <joris@vdoever.eu>
2016-06-14 14:35:36 +0200

    Refactor runner script for clarity and mas prep.
    The changed masfile is now moved to the same directory as the original
    so it can access the required goal files.
    
    Also extracted parts of the main run script as functions for clarity of
    purpose and limiting variable scopes.

--- scripts/runner.py
+++ scripts/runner.py
@@ -13,11 +13,43 @@ CMD = 'java -cp {runtime} goal.tools.Run "{mas}" --repeats {repeats} --timeout {
 BW4T_CMD = 'java -jar {bw4t}'
 DEFAULT_RUNTIME = 'runtime.jar'
 DEFAULT_BW4T = 'bw4t.jar'
-DEFAULT_REPEATS = 1 # No repeats
-DEFAULT_TIMEOUT = 300 # five minutes]
+DEFAULT_REPEATS = 1  # No repeats
+DEFAULT_TIMEOUT = 300  # five minutes]
 
 
-def detect_early_stop(log_lines, agent_count):
+def prepare_mas(maslocation, agent_count):
+    mas_file = open(maslocation)
+    mas_contents = mas_file.readlines()
+    changed_mas = tempfile.NamedTemporaryFile(delete=False)
+    replacement_count = 'agentcount = "%s"' % agent_count
+    for line in mas_contents:
+        if 'agentcount' in line:
+            line = re.sub(r'agentcount ?= ?["\']\d+["\']]', replacement_count, line)
+        changed_mas.write(line)
+    changed_mas.close()
+    dest_name = os.path.join(os.path.dirname(maslocation), os.path.basename(changed_mas.name))
+    os.rename(changed_mas.name, dest_name)
+    return dest_name
+
+
+def prepare_logfile(bw4t_logs):
+    tries = 0
+    log_file = None
+    while log_file is None:
+        tries += 1
+        if tries > 10:
+            print "Could not open log file after ten tries."
+            break
+        import time
+        time.sleep(1)
+        for listed_file in os.listdir(bw4t_logs):
+            if listed_file.endswith(".log"):
+                log_file = open(os.path.join(bw4t_logs, listed_file))
+                break
+    return log_file
+
+
+def detect_early_stop(log_lines, agents, agent_count):
     for logline in log_lines:
         logline = logline.split()
         if logline[2] != 'action':
@@ -38,35 +70,12 @@ def detect_early_stop(log_lines, agent_count):
     return False
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run BW4T.')
-    parser.add_argument('--bw4t', type=str, default=DEFAULT_BW4T,
-                        nargs=1, help='The location for the BW4T server.')
-    parser.add_argument('--runtime', type=str, default=DEFAULT_RUNTIME,
-                        nargs=1, help='The location for the GOAL runtime.')
-    parser.add_argument('--repeats', type=int, default=DEFAULT_REPEATS, nargs=1,
-                        help='How many times should each mas file be tested per com '
-                             'failure mode.')
-    parser.add_argument('--timeout', type=int, default=DEFAULT_TIMEOUT,
-                        help='What is the timeout per run.')
-    parser.add_argument('mas', type=str, default=DEFAULT_RUNTIME, nargs='+',
-                        help='The location for the MAS files to be run. '
-                        'Multiple can be provided which will be run in sequence.')
-    args = parser.parse_args()
-
-    DEVNULL = open(os.devnull, 'wb')
+def run(args):
     for maslocation in args.mas:
         for agent_count in AGENT_COUNTS:
-            mas_file = open(maslocation, )
-            mas_contents = mas_file.readlines()
-            changed_mas = tempfile.NamedTemporaryFile(delete=False)
-            replacement_count = 'agentcount = "%s"' % agent_count
-            for line in mas_contents:
-                if 'agentcount' in line:
-                    line = re.sub(r'agentcount ?= ?["\']\d+["\']]', replacement_count, line)
-                changed_mas.write(line)
-            masfile = changed_mas.name
-            changed_mas.close()
+            mas_name = os.path.basename(maslocation)
+            masfile = prepare_mas(maslocation, agent_count)
+
             for fail_chance in FAILURES:
                 bw4t_path = os.path.dirname(args.bw4t)
                 bw4t_logs = bw4t_path.join('log')
@@ -87,21 +96,22 @@ if __name__ == "__main__":
                 client = subprocess.Popen(shlex.split(client_command), stdout=subprocess.PIPE,
                                           stderr=subprocess.STDOUT)
                 # Setup the BW4T server log reading.
-                for file in os.listdir(bw4t_logs):
-                    if file.endswith(".log"):
-                        log_file = open(os.path.join(bw4t_logs, file))
+                log_file = prepare_logfile(bw4t_logs)
+
                 agents = {}
+                # Process runtime data.
                 while True:
                     line = client.stdout.readline()
                     serverlines = server.stdout.readline()
                     # Do stuff with the runtime output here.
                     log_lines = log_file.readlines()
                     # Do cycle detection, and nothing happens detection.
-                    if args.repeats == 1 and detect_early_stop(log_lines, agent_count):
+                    if args.repeats == 1 and detect_early_stop(log_lines, agents, agent_count):
                         break
 
+                    # Detect the end of the run.
                     poll = client.poll()
-                    if line.endswith('milliseconds to run jobs.') or poll == None:
+                    if line.endswith('milliseconds to run jobs.') or poll is None:
                         print line, poll
                         break
 
@@ -109,13 +119,25 @@ if __name__ == "__main__":
                 server.terminate()
                 client.communicate()
 
-                mas_name = os.path.basename(masfile)
-                os.renames(bw4t_logs, os.path.join(mas_name, '%.2f' % (fail_chance/100.0)))
-                print 'Ran {mas} with the following failure chance: {chance}'.format(mas=masfile,
+                os.renames(bw4t_logs, os.path.join(mas_name, '%.2f' % (fail_chance / 100.0)))
+                print 'Ran {mas} with the following failure chance: {chance}'.format(mas=mas_name,
                                                                                      chance=fail_chance)
-            os.remove(changed_mas)
+            os.remove(masfile)
     print 'Finished doing runs.'
 
 
-
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run BW4T.')
+    parser.add_argument('--bw4t', type=str, default=DEFAULT_BW4T,
+                        nargs=1, help='The location for the BW4T server.')
+    parser.add_argument('--runtime', type=str, default=DEFAULT_RUNTIME,
+                        nargs=1, help='The location for the GOAL runtime.')
+    parser.add_argument('--repeats', type=int, default=DEFAULT_REPEATS, nargs=1,
+                        help='How many times should each mas file be tested per com '
+                             'failure mode.')
+    parser.add_argument('--timeout', type=int, default=DEFAULT_TIMEOUT,
+                        help='What is the timeout per run.')
+    parser.add_argument('mas', type=str, nargs='+',
+                        help='The location for the MAS files to be run. '
+                        'Multiple can be provided which will be run in sequence.')
+    run(parser.parse_args())
