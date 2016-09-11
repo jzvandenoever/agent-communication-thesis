9534353378c140dd88506e81846700230a0ff733
Joris Z. van den Oever <joris@vdoever.eu>
2016-09-12 01:50:20 +0200

    redo goal system (not advanced agents yet)
    redid the way the agents choose what blocks to pick up and move around
    based on feedback. Breaks the advanced agents. Will be reworked later.
    
    Figure out a better way to make agents stop once delivery is done.
    Apparently goal-a doesn't work for that.

--- agents/common/commonKnowledge.pl
+++ agents/common/commonKnowledge.pl
@@ -10,36 +10,17 @@
 :- dynamic(holding/2).
 :- dynamic(player/1).
 :- dynamic(sequence/1).
-:- dynamic(seqDone/1).
 :- dynamic(sequenceIndex/1).
 :- dynamic(ahead/1).
 :- dynamic(grabbing/1).
 :- dynamic(grabbing/2).
 :- dynamic(canGrab/2).
+:- dynamic(delivered/1).
 
 % LookAhead agent related knowledge
 % Returns the amount of agents that we received a message of. This is the agents that we know of.
 % By not making it static it easily adapts to differing team sizes.
 agentCount(N) :- findall(Player, player(Player), Players), length(Players, N).
-
-% The subset of the sequence from the current needed block with the next N blocks. Where N is 
-% agentCount(N).
-interestingColours(CList) :- stillNeededColours(List), agentCount(N), length(CList, N),
-	append(CList, _, List).
-stillNeededColours(CList) :- seqDone(Done), sequence(Seq), append(Done, CList, Seq).
-% If we are not holding something we check what colours that are interesting are already being held.
-% When one colour is found that is not already picked up or going to be picked up at the needed amount,
-% it is wanted. If we are holding that colour we can check if it is wanted by providing the colour of
-% that block
-wantColour(ColourID) :- interestingColours(Colours), member(ColourID, Colours), 
-	countOccurence(Colours, ColourID, N), 
-	findall(ABlock, ((holding(_, ABlock); grabbing(_,ABlock)), block(ABlock, ColourID, _)), Blocks), 
-	length(Blocks, M), M<N.
-%wantColour(ColourID) :- interestingColours(Colours), member(ColourID, Colours), 
-%	countOccurence(Colours, ColourID, N).
-
-% The block being held is a block that is wanted.
-holdingWantBlock :- holding(BlockID),block(BlockID, ColorID, _), wantColour(ColorID).
 
 % Counts the amount of times X is in list.
 countOccurence([],X,0).
@@ -53,11 +34,11 @@ room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
 dropZone('DropZone').
 
 % Block information.
-% Is the agent holding the next needed block.
-holdingNextBlock :- holding(BlockID),block(BlockID, ColorID, Place), nextNeededColor(ColorID).
-% What is the current colour that needs to be dropped off.
-nextNeededColor(ColorID) :- sequence(Seq), seqDone(SDone), append(SDone, [ColorID|_], Seq).
-
-% This detects if the agents has finished. 
-%Is used to exit immediately once the goal is complete by any agent.
-finished :- sequence(Seq), seqDone(Seq).
+% Gives a list of the first X blocks to be delivered in order. If X is larger than the amount of blocks
+% to be delivered it just returns all blocks that still need to be delivered.
+nextXColoursToDeliver(Colors, X, Seq) :- sequence(SDone), append(SDone, Remainder, Seq),
+	length(Colors, X), append(Colors, _, Remainder).
+nextXColoursToDeliver(Remainder, X, Seq) :- sequence(SDone), append(SDone, Remainder, Seq),
+	length(Remainder, Y), Y<X.
+% Since we often just need the colour to deliver now here's a shortcut for that.
+colorToDeliver(ColorID, Seq) :- nextXColoursToDeliver([ColorID], 1, Seq).
