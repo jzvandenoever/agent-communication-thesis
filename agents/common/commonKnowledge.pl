a59a722e00257c53a2f909d7ce4b5a6efd68bb7b
Joris Z. van den Oever <joris@vdoever.eu>
2016-09-26 22:04:50 +0200

    Incorporating rework feedback.
    Removed unused code. Improved naming and fixed one bug.

--- agents/common/commonKnowledge.pl
+++ agents/common/commonKnowledge.pl
@@ -22,11 +22,6 @@
 % By not making it static it easily adapts to differing team sizes.
 agentCount(N) :- findall(Player, player(Player), Players), length(Players, N).
 
-% Counts the amount of times X is in list.
-countOccurence([],X,0).
-countOccurence([X|T],X,Y):- countOccurence(T,X,Z), Y is 1+Z.
-countOccurence([X1|T],X,Z):- X1\=X,countOccurence(T,X,Z).
-
 % A room is a place with exactly one neighbour, i.e., there is only one way to get 
 % to and from that place.
 room(PlaceID) :- zone(_,PlaceID,_,_,Neighbours), length(Neighbours,1).
@@ -36,9 +31,9 @@ dropZone('DropZone').
 % Block information.
 % Gives a list of the first X blocks to be delivered in order. If X is larger than the amount of blocks
 % to be delivered it just returns all blocks that still need to be delivered.
-nextXColoursToDeliver(Colors, X, Seq) :- sequence(SDone), append(SDone, Remainder, Seq),
+nextXColoursInSeq(Colors, X, Seq) :- sequence(SDone), append(SDone, Remainder, Seq),
 	length(Colors, X), append(Colors, _, Remainder).
-nextXColoursToDeliver(Remainder, X, Seq) :- sequence(SDone), append(SDone, Remainder, Seq),
+nextXColoursInSeq(Remainder, X, Seq) :- sequence(SDone), append(SDone, Remainder, Seq),
 	length(Remainder, Y), Y<X.
 % Since we often just need the colour to deliver now here's a shortcut for that.
-colorToDeliver(ColorID, Seq) :- nextXColoursToDeliver([ColorID], 1, Seq).
+nextColorInSeq(ColorID, Seq) :- nextXColoursInSeq([ColorID], 1, Seq).
